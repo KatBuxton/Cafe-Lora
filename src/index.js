94e67161dbc8bb08d8650cd549a315ea9fc599a1
Katerina Tuckova <tuckova.mail@gmail.com>
2022-04-30 11:41:15 +0200

    js change drink status to ordered

--- src/index.js
+++ src/index.js
@@ -17,5 +17,24 @@ const closeMenu = () => {
 document.querySelectorAll("a").forEach((item) => {
 item.addEventListener("click", closeMenu)
 })
+const orderBtn = document.querySelector(".order-btn")
+
+const drinkElm = document.querySelector(".drink__cup")
+
+let ordered = false 
+
+orderBtn.addEventListener("click", () => {
+  if (ordered === false) {
+    ordered = true;
+    drinkElm.classList.add("drink__cup--selected");
+    orderBtn.textContent = "Zrušit";
+  }
+  else {
+    ordered = false;
+    drinkElm.classList.remove("drink__cup--selected");
+    orderBtn.textContent = "Objednat";
+  }
+})
+
 
 
