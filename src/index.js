7cb17f336a98f67d133072bf8a5184d90b6aad06
Katerina Tuckova <tuckova.mail@gmail.com>
2022-04-30 11:16:08 +0200

    js opens and closes nav menu items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.querySelector("#nav-btn")
+const navElm = document.querySelector("nav")
+
+const toggleMenu = () => {
+navElm.classList.toggle("nav-closed");
+}
+navBtn.addEventListener("click", toggleMenu)
+
+const closeMenu = () => {
+  navElm.classList.add("nav-closed");
+ }
+
+document.querySelectorAll("a").forEach((item) => {
+item.addEventListener("click", closeMenu)
+})
+
+
